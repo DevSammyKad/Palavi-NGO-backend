8c19fc62750b2ecef15da282e1de608c51ef0733
DevSammyKad <DevSammyKad@gmail.com>
2025-04-23 01:27:08 +0530

    refactor donationRoutes: change payment status endpoint from POST to GET for improved RESTful API design

--- routes/donationRoutes.js
+++ routes/donationRoutes.js
@@ -9,6 +9,6 @@ const router = express.Router();
 
 // POST request to save donation data
 router.post('/donations', initiatePayment);
-router.post('/donations/status/:txnId', handlePaymentStatus);
+router.get('/donations/status/:txnId', handlePaymentStatus);
 
 module.exports = router;
